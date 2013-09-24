a7e5b1caef58292242dc2c809aebb17d1524e0f6
Clark Kampfe <clark.kampfe@gmail.com>
2013-09-23 19:57:11 -0500

    refac: removed unnecessary control logic

--- 140char.rb
+++ 140char.rb
@@ -11,30 +11,20 @@ def split_encapsulation(s)
     return s
   end
   
-  # potential for refactoring: delete first lambda if block
-  
-  # doesn't really require a base case, since the lengths are computed
-  # by iteration and flow control 
   splitter = lambda do |remaining_string, a|
-    # if the 140th character is a word, take the first 140 chars as a tweet
-    if remaining_string[139] == /\W/
-      a << remaining_string.slice!(/^.{140}/) 
-      splitter.call(remaining_string, a)
-    else
-      # find the first word, and take from there to zero
-      i = 139 # start at remaining_string[139], ie length=140
-      while i > 0
-        unless remaining_string[i] =~ (/\W/)
-          i -= 1
-        else
-          a << remaining_string.slice!(/^.{#{i+1}}/) # <-- seems dirty
-          splitter.call(remaining_string, a)
-        end
-      end 
+    # find the first word, and take from there to zero
+    i = 139 # start at remaining_string[139], ie length=140
+    while i > 0
+      unless remaining_string[i] =~ (/\W/)
+        i -= 1
+      else
+        a << remaining_string.slice!(/^.{#{i+1}}/) # <-- seems dirty
+        splitter.call(remaining_string, a)
+      end
     end 
 
     return a
-  end
+  end 
   
   tweets = []
 
