99a1a858a56e337b282f24cbfe8bcf1de76b30a0
Clark Kampfe <clark.kampfe@gmail.com>
2013-11-23 12:49:38 -0600

    trying to bugfix stack too deep

--- pipetweet.rb
+++ pipetweet.rb
@@ -1,4 +1,5 @@
 require './environment'
+require './options'
 
 class Collection
 
@@ -6,16 +7,16 @@ class Collection
     @collection = tweet_collection
   end
 
-  def send_tweets
+  def send(interval)
     @collection.each do |tweet|
       puts "tweet # #{@collection.index(tweet)}"
       CLIENT.update(tweet.body)
-      sleep_15
+      sleep(interval)
     end
   end
 
-  def sleep_15
-    sleep(15)
+  def sleep(sec=15)
+    sleep(sec)
   end
 end
 
@@ -29,6 +30,10 @@ class Tweet
   end
 end
 
+### DRIVER ###
+
+options = get_opts # options from optionparser
+
 base_string = ""
 
 # so as to get multiple lines of text
@@ -36,7 +41,11 @@ while line = gets
   base_string += line
 end
 
-s = splitter(base_string)
-tweets = s.map { |t| Tweet.new(t) }
-collection = Collection.new(tweets)
-collection.send_tweets
+debugger
+s = splitter(base_string) # split string
+tweets = s.map { |t| Tweet.new(t) } # tweet objects
+collection = Collection.new(tweets) # tweet collection
+debugger
+p options
+p options[:interval]
+collection.send(options[:interval])
