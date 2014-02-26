141b4b136ff55f12a94aebd6b03f0cfd457bc62f
Clark Kampfe <clark.kampfe@gmail.com>
2014-02-26 13:46:47 -0600

    change oauth token to newer style access token

--- env.rb
+++ env.rb
@@ -17,6 +17,6 @@ end
 CLIENT = Twitter::REST::Client.new do |config|
   config.consumer_key       = ENV['TWITTER_CONSUMER_KEY']
   config.consumer_secret    = ENV['TWITTER_CONSUMER_SECRET']
-  config.oauth_token        = ENV['OAUTH_TOKEN']
-  config.oauth_token_secret = ENV['OAUTH_TOKEN_SECRET']
+  config.access_token        = ENV['ACCESS_TOKEN']
+  config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
 end
